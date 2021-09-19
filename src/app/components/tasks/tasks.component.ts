df0ddcc620b05ffbe45be4e94cd364cf8d0ca427
Aleksi Statsevich <aleksi.statsevich@gmail.com>
2021-09-19 13:02:53 +0300

    filter

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -22,7 +22,9 @@ export class TasksComponent implements OnInit {
   }
 
   deleteTask(task: Task) {
-    this.taskService.deleteTask(task).subscribe(()=>(this.tasks = this.tasks.filter(t=>t.id ! === task.id)));
+    this.taskService.deleteTask(task).subscribe(()=>(
+      this.tasks = this.tasks.filter(t=>t.id !== task.id
+    )));
   }
 
 }
