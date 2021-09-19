98572a3c8862e3711e9fe5a7c79b6f21634efa89
Aleksi Statsevich <aleksi.statsevich@gmail.com>
2021-09-19 12:34:44 +0300

    add service and a fake back end

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import {TASKS} from '../../mock-task';
-import {Task}  from '../../Task';
+import { Task }  from '../../Task';
+import { TaskService } from 'src/app/services/task.service';
+
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.scss']
 })
-export class TasksComponent implements OnInit {
-  tasks: Task[] = TASKS;
 
-  constructor() { }
+export class TasksComponent implements OnInit {
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) { }
 
   ngOnInit(): void {
+    //async
+    this.taskService.getTasks().subscribe((tasks)=>{
+      this.tasks = tasks
+    });
   }
 
 }
